60aec7b32d6d344c23a20e34491490a08cf165c8
José Guilherme Vanz <jvanz@jvanz.com>
2020-05-31 18:07:33 -0300

    Handler response request about the subscription request
    Updates the consumer program to check the message after the subscription
    request. The queue system sends a message telling the client if it is
    success fully subscribed or not in the topic.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/client/consumer.cpp
+++ src/client/consumer.cpp
@@ -7,26 +7,35 @@
 using boost::asio::ip::tcp;
 using namespace std::chrono_literals;
 
+// Forward declarion
+class Consumer;
+// Callback function used un the Consumer class methods
+using ReadCallback = std::function<void(Consumer*, Message&&)>;
+// The definiton of the default callbacks for the subscribe and read methods
+void defaultReadCallback(Consumer* consumer, Message&& msg);
+void defaultSubscribeCallback(Consumer* consumer, Message&& msg);
+
+static unsigned int messagesCount = 0;
+
 class Consumer {
  public:
-  Consumer(tcp::socket&& socket)
-      : _socket(std::move(socket)), _message_received{0} {
-    Read();
-    Subscribe();
-  }
+  Consumer(tcp::socket&& socket) : _socket(std::move(socket)){};
 
- private:
-  void Subscribe() {
+  void Subscribe(ReadCallback callback) {
     std::ostream os(&_write_buffer);
     Message msg(MessageType::SUBSCRIBE, "test");
     os << msg;
+    Read(callback);
     boost::asio::write(_socket, _write_buffer);
   };
 
-  void Read() {
+  void Read(ReadCallback callback) {
+    std::cout << "Read()" << std::endl;
     boost::asio::async_read_until(
         _socket, _read_buffer, MESSAGE_TERMINATION_CHAR,
-        [this](boost::system::error_code ec, std::size_t length) {
+        [this, callback](boost::system::error_code ec, std::size_t length) {
+          std::cout << "More data! Error: " << ec << ". Length: " << length
+                    << " bytes" << std::endl;
           if (!ec) {
             auto bufs = _read_buffer.data();
             std::istringstream is(
@@ -34,30 +43,53 @@ class Consumer {
                             boost::asio::buffers_begin(bufs) + length));
             Message msg;
             is >> msg;
-            _message_received += 1;
             _read_buffer.consume(length);
-            std::cout << "Received: " << msg.GetID() << ": " << msg.GetData()
-                      << " =--> Total: " << _message_received << std::endl;
+            callback(this, std::move(msg));
           }
-          Read();
         });
   };
 
+ private:
   boost::asio::streambuf _read_buffer;
   boost::asio::streambuf _write_buffer;
   tcp::socket _socket;
-  unsigned int _message_received;
+};
+
+void defaultReadCallback(Consumer* consumer, Message&& msg) {
+  messagesCount += 1;
+  std::cout << "Received: " << msg.GetID() << ": " << msg.GetData()
+            << " =--> Total: " << messagesCount << std::endl;
+  consumer->Read(defaultReadCallback);
+};
+
+void defaultSubscribeCallback(Consumer* consumer, Message&& msg) {
+  std::cout << "Subscribe callback : " << msg.GetTopic() << " -> "
+            << msg.GetData() << "(" << msg.GetID() << ")" << std::endl;
+  if (msg.GetType() == MessageType::SUBSCRIBED) {
+    consumer->Read(defaultReadCallback);
+  } else if (msg.GetType() == MessageType::TOPIC_NOT_FOUND) {
+    std::this_thread::sleep_for(1s);
+    consumer->Subscribe(defaultSubscribeCallback);
+  } else {
+    std::cout << "Message type not supported" << std::endl;
+  }
 };
 
 int main(int argc, char* argv[]) {
   try {
     boost::asio::io_context io_context;
+    // prevent the context to stop if there are no events in the loop
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type>
+        work = boost::asio::make_work_guard(io_context);
+
     tcp::resolver resolver(io_context);
     tcp::socket socket(io_context);
     boost::asio::connect(socket, resolver.resolve("localhost", "8888"));
 
     Consumer consumer(std::move(socket));
+    consumer.Subscribe(defaultSubscribeCallback);
     io_context.run();
+    std::cout << "Bye!" << std::endl;
 
   } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << "\n";
