4ab2f062a9b838a97cbdbd770dcedeb62cf2683d
José Guilherme Vanz <jvanz@jvanz.com>
2020-06-03 08:29:56 -0300

    Common client
    Adds a common client class to be used by the producer and consumer
    program. This class is able to connect into queue system, create and
    subscribe topic, send and receive messages.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,33 +1,56 @@
-
 #include "client.hpp"
 
-void Client::Subscribe(std::string topic, ) {
-  Message msg(MessageType::SUBSCRIBE, topic);
+Client::Client(boost::asio::io_context& io_context, std::string server,
+               std::string port)
+    : _messagesReadCount(0), _messagesWriteCount(0), _socket(io_context) {
+  tcp::resolver resolver(io_context);
+  boost::asio::connect(_socket, resolver.resolve(server, port));
+};
+
+void Client::Subscribe(std::string topic, ReadCallback callback) {
+  SubscriptionRequestMessage msg(topic);
+  Read(callback);
+  Write(std::move(msg));
+};
+
+void Client::Read(ReadCallback callback) {
+  std::cout << "Read()" << std::endl;
+  boost::asio::async_read_until(
+      _socket, _read_buffer, MESSAGE_TERMINATION_CHAR,
+      [this, callback](boost::system::error_code ec, std::size_t length) {
+        std::cout << "More data! Error: " << ec << ". Length: " << length
+                  << " bytes" << std::endl;
+        if (!ec) {
+          auto bufs = _read_buffer.data();
+          std::istringstream is(
+              std::string(boost::asio::buffers_begin(bufs),
+                          boost::asio::buffers_begin(bufs) + length));
+          Message msg;
+          is >> msg;
+          _read_buffer.consume(length);
+          _messagesReadCount += 1;
+          callback(this, std::move(msg));
+        }
+      });
+};
+
+void Client::CreateTopic(std::string topic, ReadCallback callback) {
+  CreateTopicMessage msg(topic);
+  Read(callback);
   Write(std::move(msg));
 }
 
-void Client::Publish(std::string topic, std::string data) {}
-
-Message&& Client::(std::string topic) {}
-
-void Client::Write(Message&& msg) {
-  std::ostream os(&_write_buffer);
-  os << msg;
-  std::cout << "Message sent: " << msg << std::endl;
-  boost::asio::write(_socket, _write_buffer);
+void Client::PublishMessage(std::string topic, std::string data) {
+  Message message(MessageType::SEND, topic, data);
+  Write(std::move(message));
 }
 
-void Client::Read() {
-  _socket.async_read_some(
-      _read_buffer.prepare(514),
-      [this](boost::system::error_code ec, std::size_t length) {
-        if (!ec) {
-          std::istream is(&_read_buffer);
-          _read_buffer.commit(length);
-          Message msg;
-          is >> msg;
-          std::cout << "Received: " << msg << std::endl;
-        }
-        Read();
-      });
+void Client::Write(Message&& message) {
+  boost::asio::streambuf buffer;
+  std::ostream os2(&buffer);
+  os2 << message;
+  auto length = boost::asio::write(_socket, buffer);
+  _messagesWriteCount += 1;
+  std::cout << "Sent: " << message.GetID() << ". Length: " << length + 1
+            << ". Count: " << _messagesWriteCount << std::endl;
 }
--- src/client/client.hpp
+++ src/client/client.hpp
@@ -7,22 +7,57 @@
 using boost::asio::ip::tcp;
 using namespace std::chrono_literals;
 
+class Client;
+// Callback function type used in the Client class methods
+using ReadCallback = std::function<void(Client*, Message&&)>;
+
 /**
- * Class to stablish and send message to queue system
+ * Class with all the code to send and read data to the queue server.
  */
 class Client {
  public:
-  Client(tcp::socket&& socket) : _socket(std::move(socket)){};
-
-  void Subscribe(std::string topic);
-  void Publish(std::string topic, std::string data);
-  void Receive(std::string topic);
+  Client(boost::asio::io_context& io_context, std::string server,
+         std::string port);
+  /**
+   * Send a subscription request to the server.
+   *
+   * @param topic topic name which the client would like to register to
+   * @param callback callback functions called when the client receive a
+   * response about the subscription request
+   */
+  void Subscribe(std::string topic, ReadCallback callback);
+  /**
+   * Register a callback to messages from the server
+   *
+   * @param callback Callback function called for any message received
+   */
+  void Read(ReadCallback callback);
+  /**
+   * Send a message to the server to create a new topic.
+   *
+   * @param topic The topic name which the client would like to create
+   * @param callback callback function called when the client receive a response
+   * about the topic creation request
+   */
+  void CreateTopic(std::string topic, ReadCallback callback);
+  /**
+   * Publish a message in a topic
+   *
+   * @param topic topic name where the given data should be published
+   * @param data data to be published
+   */
+  void PublishMessage(std::string topic, std::string data);
 
  private:
-  void Write();
-  void Read();
-
+  /**
+   * Write a message in the socket to the server
+   *
+   * @param message message to be sent
+   */
+  void Write(Message&& message);
   boost::asio::streambuf _read_buffer;
   boost::asio::streambuf _write_buffer;
   tcp::socket _socket;
+  unsigned int _messagesWriteCount;
+  unsigned int _messagesReadCount;
 };
