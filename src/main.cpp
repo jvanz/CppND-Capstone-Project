7a887739ee6130e675148aeb32526a589f417c76
José Guilherme Vanz <jvanz@jvanz.com>
2020-06-06 23:04:08 -0300

    Allow user set the listen port
    Update the main of the queue system program allowing the user specify
    which port the program should use to wait for new connections.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,17 @@ using boost::asio::ip::tcp;
 
 int main(int argc, char* argv[]) {
   try {
+    // get the data necessary to run
+    if (argc != 2) {
+      std::cerr << "Invalid arguments. Expected: queue <listen port>"
+                << std::endl;
+      return -1;
+    }
+    std::string port(argv[1]);
+
     boost::asio::io_context io_context;
     // start the server which listen for new connections
-    Server s(io_context, 8888);
+    Server s(io_context, std::stoi(port));
     io_context.run();
   } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << "\n";
