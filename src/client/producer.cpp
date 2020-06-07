da01d60dfa0ef84d99153b5ce1b6848a2e912c18
José Guilherme Vanz <jvanz@jvanz.com>
2020-06-07 18:16:42 -0300

    Read messages from file
    Updates the producer program to read the message send to the queue
    system from a file.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/client/producer.cpp
+++ src/client/producer.cpp
@@ -1,4 +1,5 @@
 #include <boost/asio.hpp>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -9,22 +10,20 @@
 using boost::asio::ip::tcp;
 using namespace std::chrono_literals;
 
-// total number of messages send by the program to the queue system
-constexpr unsigned int TOTAL_MESSAGES_COUNT = 1000;
-
 // function used to publish messages
-static void publishMessage(Client* producer, Message&& confirmationMessage) {
-  for (auto x = 0; x < TOTAL_MESSAGES_COUNT; x++) {
-    auto time = 10ms;
-    std::this_thread::sleep_for(time);
-    producer->PublishMessage(confirmationMessage.GetTopic(),
-                             "message " + std::to_string(x));
+static void publishMessage(Client* producer, Message&& confirmationMessage,
+                           std::string& messagesfilepath) {
+  std::ifstream messagesfile(messagesfilepath);
+  if (messagesfile.is_open()) {
+    for (std::string line; std::getline(messagesfile, line);) {
+      producer->PublishMessage(confirmationMessage.GetTopic(), line);
+    }
   }
 }
 
 int main(int argc, char* argv[]) {
   // get the data necessary to run
-  if (argc != 4) {
+  if (argc != 5) {
     std::cerr << "Invalid arguments. Expected: producer <server address> "
                  "<server port> <topic to publish messages>"
               << std::endl;
@@ -33,6 +32,7 @@ int main(int argc, char* argv[]) {
   std::string server(argv[1]);
   std::string port(argv[2]);
   std::string topic(argv[3]);
+  std::string messagesfilepath(argv[4]);
 
   try {
     boost::asio::io_context io_context;
@@ -44,18 +44,20 @@ int main(int argc, char* argv[]) {
     Client producer(io_context, server, port);
 
     // Create the topic
-    producer.CreateTopic(topic, [](Client* client, Message&& message) {
-      if (message.GetType() == MessageType::TOPIC_CREATED) {
-        // topic created. Start a task to publish the messages
-        std::cout << "Topic created" << std::endl;
-        auto future = std::async(publishMessage, client, std::move(message));
-        future.wait();
-      } else if (message.GetType() == MessageType::TOPIC_CREATED) {
-        std::cout << "Cannot create the topic" << std::endl;
-      } else {
-        std::cout << "Message type not supported" << std::endl;
-      }
-    });
+    producer.CreateTopic(
+        topic, [&messagesfilepath](Client* client, Message&& message) {
+          if (message.GetType() == MessageType::TOPIC_CREATED) {
+            // topic created. Start a task to publish the messages
+            std::cout << "Topic created" << std::endl;
+            auto future = std::async(publishMessage, client, std::move(message),
+                                     std::ref(messagesfilepath));
+            future.wait();
+          } else if (message.GetType() == MessageType::TOPIC_CREATED) {
+            std::cout << "Cannot create the topic" << std::endl;
+          } else {
+            std::cout << "Message type not supported" << std::endl;
+          }
+        });
 
     io_context.run();
     std::cout << "Bye!" << std::endl;
