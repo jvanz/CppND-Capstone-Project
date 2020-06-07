72b260faf71721a00dace99781b2a65a9df49311
José Guilherme Vanz <jvanz@jvanz.com>
2020-06-06 23:01:01 -0300

    Move operator and constructor for message queue
    Updates the MessageQueue class adding the move constructor and
    assignment operator. Furthermore, change the notify_one to notify_all.
    Thus, all the threads waiting for a message will be notified.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/message_queue.hpp
+++ src/message_queue.hpp
@@ -11,12 +11,22 @@
 template <class T>
 class MessageQueue {
  public:
-	 // TODO rule of five
   MessageQueue(){};
-  MessageQueue(MessageQueue<T> &&other) noexcept {};
-  MessageQueue(const MessageQueue<T> &other) noexcept {};
-  MessageQueue &operator=(MessageQueue<T> &other) noexcept {};
-  MessageQueue &operator=(const MessageQueue<T> &&other) noexcept {};
+  MessageQueue(MessageQueue<T> &&other) noexcept {
+    _mutex = std::move(other._mutex);
+    _cv = std::move(other._cv);
+    _queue = std::move(other._queue);
+  };
+  MessageQueue &operator=(MessageQueue<T> &&other) noexcept {
+    _mutex = std::move(other._mutex);
+    _cv = std::move(other._cv);
+    _queue = std::move(other._queue);
+    return *this;
+  };
+  ~MessageQueue(){};
+  // Cannot be copied
+  MessageQueue(const MessageQueue<T> &other) = delete;  //
+  MessageQueue &operator=(MessageQueue<T> &other) = delete;
 
   /**
    * Adds a new item in the queue
@@ -24,31 +34,27 @@ class MessageQueue {
   void Send(T &&msg) {
     std::lock_guard<std::mutex> lock(_mutex);
     _queue.push(std::move(msg));
-    cv.notify_one();
+    _cv.notify_all();
   };
   /**
    * Remove the first item in the queue
    */
   T Receive() {
     std::unique_lock<std::mutex> lock(_mutex);
-    cv.wait(lock, [&] { return _queue.size() > 0; });
+    _cv.wait(lock, [&] { return _queue.size() > 0; });
 
     auto next = _queue.front();
     _queue.pop();
 
     lock.unlock();
-    cv.notify_one();
+    _cv.notify_one();
     return next;
   };
 
  private:
-  // TODO needs to be static?
-  static std::mutex _mutex;  // cannot access the queue concurrently
-  std::condition_variable cv;
+  std::mutex _mutex;  // cannot access the queue concurrently
+  std::condition_variable _cv;
   std::queue<T> _queue;  // pending messages
 };
-
-template <class T>
-std::mutex MessageQueue<T>::_mutex;
 
 #endif
