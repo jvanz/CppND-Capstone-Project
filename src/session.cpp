07be2d2f68e9f95ce6f32d14dd00f64b53097624
José Guilherme Vanz <jvanz@jvanz.com>
2020-06-07 20:26:17 -0300

    Fix coredump
    When the client close the connection the server needs to close the
    socket. This commit does that. When an error on writing to the socket
    happen, the socket is closed and the thread is finished.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/session.cpp
+++ src/session.cpp
@@ -1,6 +1,8 @@
 #include <common/message.hpp>
+#include <exception>
 #include <future>
 #include <iostream>
+#include <stdexcept>
 
 #include "session.hpp"
 
@@ -31,6 +33,9 @@ void Session::Read() {
               std::async(std::launch::async, &Server::ProcessPendingMessage,
                          _server, std::move(msg), std::ref(*this));
           result.wait();
+        } else {
+          std::cerr << "ERROR: " << std::endl;
+          return;
         }
         Read();
       });
@@ -40,11 +45,19 @@ void Session::Write(Message& msg) {
   boost::asio::streambuf b;
   std::ostream os(&b);
   os << msg;
-  auto bytes_count = _socket->send(b.data());
-  std::cout << "Message sent: " << msg.GetID() << ". " << bytes_count
-            << " bytes -> Type: " << msg.GetType()
-            << ", topic: " << msg.GetTopic() << ", data: " << msg.GetData()
-            << std::endl;
+  try {
+    auto bytes_count = _socket->send(b.data());
+    std::cout << "Message sent: " << msg.GetID() << ". " << bytes_count
+              << " bytes -> Type: " << msg.GetType()
+              << ", topic: " << msg.GetTopic() << ", data: " << msg.GetData()
+              << std::endl;
+  } catch (...) {
+    // It's not possible to send the message.Shutdown the socket
+    std::cerr << "Cound not send: " << msg.GetData() << std::endl;
+    _socket->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+    _socket->close();
+    std::rethrow_exception(std::current_exception());
+  }
 }
 
 void Session::Write(Message&& msg) { Write(msg); }
@@ -52,9 +65,16 @@ void Session::Write(Message&& msg) { Write(msg); }
 void Session::Listen(Topic& topic) {
   _listeningThreads.emplace_back([this, &topic]() {
     using namespace std::chrono_literals;
-    for (;;) {
+    while (_socket->is_open()) {
       auto message = topic.Receive();
-      Write(message);
+      try {
+        Write(message);
+      } catch (...) {
+        // It's not possible to send the message. Insert it again in the queue
+        // to be processed
+        topic.Send(std::move(message));
+        return;
+      }
       std::this_thread::sleep_for(1ms);
     }
   });
